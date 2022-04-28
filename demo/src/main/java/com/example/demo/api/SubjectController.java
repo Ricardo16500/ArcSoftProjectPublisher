c356212ba3bc3702843725889a62fb75e22248a3
Ricardo16500 <fabiorvm16500@gmail.com>
2022-04-28 10:28:45 -0400

    finalizando publisher

--- demo/src/main/java/com/example/demo/api/SubjectController.java
+++ demo/src/main/java/com/example/demo/api/SubjectController.java
@@ -1,15 +1,18 @@
 package com.example.demo.api;
 
 import com.example.demo.bl.SubjectBl;
+import com.example.demo.config.RabbitMqConfig;
 import com.example.demo.entity.Student;
 import com.example.demo.entity.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,11 +30,19 @@ public class SubjectController {
         this.subjectBl = subjectBl;
     }
 
+    @Autowired
+    private RabbitTemplate template;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Subject>> getSubjects() {
         LOGGER.info("Invocando al servicio REST para obtener el listado de materias");
         List<Subject> subjectList = subjectBl.getSubjects();
         LOGGER.info("Invocacion exitosa para obtener el listado de materias {}", subjectList);
+
+        template.convertAndSend(RabbitMqConfig.DIRECT_EXCHANGE,RabbitMqConfig.ROUTING_KEY_3, subjectList);
+        template.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "", subjectList);
+        template.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE,RabbitMqConfig.T_ROUTING_KEY2,subjectList);
+
         return new ResponseEntity<>(subjectList, HttpStatus.OK);
     }
 
@@ -39,9 +50,13 @@ public class SubjectController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Subject> getSubjectsById(@PathVariable Integer id) throws Exception {
         LOGGER.info("Invocando al servicio REST para obtener la materia con id: {}", id);
-        Subject subject = subjectBl.getSubjectById(id);
-        LOGGER.info("Invocacion exitosa para obtener la materia: {}", subject);
-        return new ResponseEntity<>(subject, HttpStatus.OK);
+        Subject subjectList = subjectBl.getSubjectById(id);
+
+        template.convertAndSend(RabbitMqConfig.DIRECT_EXCHANGE,RabbitMqConfig.ROUTING_KEY_3, subjectList);
+        template.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "", subjectList);
+        template.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE,RabbitMqConfig.T_ROUTING_KEY2,subjectList);
+
+        return new ResponseEntity<>(subjectList, HttpStatus.OK);
     }
 
 
@@ -49,6 +64,13 @@ public class SubjectController {
     public ResponseEntity<Subject> saveSubject(@RequestBody Subject subject) {
         LOGGER.info("Invocando al servicio REST para registrar una materia con la siguiente información: {}", subject);
         Subject result = subjectBl.saveSubject(subject);
+        List<Subject> subjectList = new ArrayList<>();
+        subjectList.add(result);
+
+        template.convertAndSend(RabbitMqConfig.DIRECT_EXCHANGE,RabbitMqConfig.ROUTING_KEY_3, subjectList);
+        template.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "", subjectList);
+        template.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE,RabbitMqConfig.T_ROUTING_KEY2,subjectList);
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- demo/src/main/java/com/example/demo/api/TeacherController.java
+++ demo/src/main/java/com/example/demo/api/TeacherController.java
@@ -2,14 +2,17 @@ package com.example.demo.api;
 
 import com.example.demo.bl.SubjectBl;
 import com.example.demo.bl.TeacherBl;
+import com.example.demo.config.RabbitMqConfig;
 import com.example.demo.entity.Teacher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,11 +30,20 @@ public class TeacherController {
         this.teacherBl = teacherBl;
     }
 
+    @Autowired
+    private RabbitTemplate template;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Teacher>> getSubjects() {
         LOGGER.info("Invocando al servicio REST para obtener el listado de profesores");
         List<Teacher> teacherList = teacherBl.getTeachers();
         LOGGER.info("Invocacion exitosa para obtener el listado de materias {}", teacherList);
+
+        template.convertAndSend(RabbitMqConfig.DIRECT_EXCHANGE, RabbitMqConfig.ROUTING_KEY_2,teacherList);
+        template.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "",teacherList);
+        template.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE, RabbitMqConfig.T_ROUTING_KEY3,teacherList);
+
+
         return new ResponseEntity<>(teacherList, HttpStatus.OK);
     }
 
@@ -40,7 +52,15 @@ public class TeacherController {
     public ResponseEntity<Teacher> getSubjectsById(@PathVariable Integer id) throws Exception {
         LOGGER.info("Invocando al servicio REST para obtener en profesor con id: {}", id);
         Teacher teacher = teacherBl.getTeacherById(id);
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(teacher);
         LOGGER.info("Invocacion exitosa para obtener la materia: {}", teacher);
+
+        template.convertAndSend(RabbitMqConfig.DIRECT_EXCHANGE, RabbitMqConfig.ROUTING_KEY_2,teacherList);
+        template.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "",teacherList);
+        template.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE, RabbitMqConfig.T_ROUTING_KEY3,teacherList);
+
+
         return new ResponseEntity<>(teacher, HttpStatus.OK);
     }
 
@@ -49,6 +69,13 @@ public class TeacherController {
     public ResponseEntity<Teacher> saveSubject(@RequestBody Teacher teacher) {
         LOGGER.info("Invocando al servicio REST para registrar un profesor con la siguiente información: {}", teacher);
         Teacher result = teacherBl.saveTeacher(teacher);
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(result);
+
+        template.convertAndSend(RabbitMqConfig.DIRECT_EXCHANGE, RabbitMqConfig.ROUTING_KEY_2,teacherList);
+        template.convertAndSend(RabbitMqConfig.FANOUT_EXCHANGE, "",teacherList);
+        template.convertAndSend(RabbitMqConfig.TOPIC_EXCHANGE, RabbitMqConfig.T_ROUTING_KEY3,teacherList);
+
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
