8410f46c89836553d85736f4927165b059117d44
Ricardo16500 <fabiorvm16500@gmail.com>
2022-04-27 23:03:56 -0400

    add all exchanges

--- demo/src/main/java/com/example/demo/config/RabbitMqConfig.java
+++ demo/src/main/java/com/example/demo/config/RabbitMqConfig.java
@@ -15,11 +15,17 @@ import static org.springframework.amqp.core.BindingBuilder.bind;
  */
 @Configuration
 public class RabbitMqConfig {
-
+    /*
+    * Queue test
+    *
+     */
     public static final String QUEUE = "test_queue";
     public static final String EXCHANGE = "test_exchange";
     public static final String ROUTING_KEY = "test_routing_key";
 
+    /*
+    * Direct exchange values
+     */
     public static final String DIRECT_EXCHANGE = "direct_exchange";
     public static final String D_QUEUE1 = "student_queue";
     public static final String ROUTING_KEY_1 = "student";
@@ -30,10 +36,29 @@ public class RabbitMqConfig {
     public static final String D_QUEUE3 = "subject_queue";
     public static final String ROUTING_KEY_3 = "subject";
 
-    public static final String F_QUEUE_1 = "fantout_queue_1";
-    public static final String F_QUEUE_2 = "fantout_queue_2";
-    public static final String F_QUEUE_3 = "fantout_queue_3";
+    /*
+    * Fanout exchange values
+     */
+    public static final String F_QUEUE_1 = "fanout_queue_1";
+    public static final String F_QUEUE_2 = "fanout_queue_2";
+    public static final String F_QUEUE_3 = "fanout_queue_3";
     public static final String FANOUT_EXCHANGE = "fanout_exchange";
+
+    /*
+    * Topic exchange values
+     */
+    public static final String T_QUEUE_1 = "t_student_queue";
+    public static final String T_QUEUE_2 = "t_subject_queue";
+    public static final String T_QUEUE_3 = "t_teacher_queue";
+    public static final String T_QUEUE_4 = "t_total_queue";
+    public static final String T_QUEUE_5 = "t_student_teacher_queue";
+    public static final String TOPIC_EXCHANGE = "topic_exchange";
+
+    public static final String T_ROUTING_KEY1= "topic.student";
+    public static final String T_ROUTING_KEY2= "topic.subject";
+    public static final String T_ROUTING_KEY3= "topic.teacher";
+    public static final String T_ROUTING_KEY4= "topic.*";
+    public static final String T_ROUTING_KEY5= "topic.#";
 
     /*
     *
@@ -144,4 +169,68 @@ public class RabbitMqConfig {
     }
 
 
+    /*
+    * Topic exchange
+     */
+    @Bean
+    public TopicExchange topicExchange(){
+        return new TopicExchange(TOPIC_EXCHANGE);
+    }
+
+    @Bean
+    public Queue teQueue(){
+        return new Queue(T_QUEUE_1);
+    }
+    @Bean
+    public Queue teQueue2(){
+        return new Queue(T_QUEUE_2);
+    }
+    @Bean
+    public Queue teQueue3(){
+        return new Queue(T_QUEUE_3);
+    }
+    @Bean
+    public Queue teQueue4(){
+        return new Queue(T_QUEUE_4);
+    }
+    @Bean
+    public Queue teQueue5(){
+        return new Queue(T_QUEUE_5);
+    }
+
+    @Bean
+    public Binding tBinding (Queue teQueue, TopicExchange topicExchange){
+        return BindingBuilder
+                .bind(teQueue)
+                .to(topicExchange)
+                .with(T_ROUTING_KEY1);
+    }
+    @Bean
+    public Binding tBinding2 (Queue teQueue2, TopicExchange topicExchange){
+        return BindingBuilder
+                .bind(teQueue2)
+                .to(topicExchange)
+                .with(T_ROUTING_KEY2);
+    }
+    @Bean
+    public Binding tBinding3 (Queue teQueue3, TopicExchange topicExchange){
+        return BindingBuilder
+                .bind(teQueue3)
+                .to(topicExchange)
+                .with(T_ROUTING_KEY3);
+    }
+    @Bean
+    public Binding tBinding4 (Queue teQueue4, TopicExchange topicExchange){
+        return BindingBuilder
+                .bind(teQueue4)
+                .to(topicExchange)
+                .with(T_ROUTING_KEY4);
+    }
+    @Bean
+    public Binding tBinding5 (Queue teQueue5, TopicExchange topicExchange){
+        return BindingBuilder
+                .bind(teQueue5)
+                .to(topicExchange)
+                .with(T_ROUTING_KEY5);
+    }
 }
